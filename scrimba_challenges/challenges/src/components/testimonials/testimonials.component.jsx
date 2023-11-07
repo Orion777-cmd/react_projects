aa2a35812f67f498d462cb317c435f4c081b55e2
Orion777-cmd <abiy.biru78@gmail.com>
2023-11-08 00:16:37 +0300

    moved the banners and testimonials render into their own page component

--- scrimba_challenges/challenges/src/components/testimonials/testimonials.component.jsx
+++ scrimba_challenges/challenges/src/components/testimonials/testimonials.component.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import "./testimonials.styles.scss";
+import QutoeLogo from "../../assets/Icon.svg"
 
 const Testimonials = () => {
     return (
-        <>
-        
-        </>
+        <div className="testimonials-container">
+            <div className="wrapper">
+                <div className="image-container">
+
+                </div>
+                <div className="details-container">
+                    <QutoeLogo />
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos minus, harum architecto et hic fugiat blanditiis, commodi perspiciatis earum aspernatur</p>
+                    <div className="personal-detail">
+                        <p>May Anderson</p>
+                        <p>Workstation, CTO</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
---
+++ scrimba_challenges/challenges/src/pages/testimonialsPage/testimonialsPage.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Testimonials from "../../components/testimonials/testimonials.component";
+
+const TestimonialsPage = () => {
+    return (
+        <>
+        
+        </>
+    )
+}
+
+export default TestimonialsPage;
